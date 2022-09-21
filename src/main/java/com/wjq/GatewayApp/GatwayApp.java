e510ccf594a89132cfc6b941b0b0184c92344b6e
wjqwjqw <99686154+wjqwjqw@users.noreply.github.com>
2022-09-21 22:51:24 +0800

    Initial commit

---
+++ src/main/java/com/wjq/GatewayApp/GatwayApp.java
@@ -0,0 +1,12 @@
+package com.wjq.GatewayApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatwayApp.class,args);
+    }
+}
